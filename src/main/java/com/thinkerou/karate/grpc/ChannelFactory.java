512304883f09eda715c0bf0b8c4457c479bbd86f
thinkerou <thinkerou@gmail.com>
2018-09-18 13:44:50 +0800

    add grpc client

--- src/main/java/com/thinkerou/karate/grpc/ChannelFactory.java
+++ src/main/java/com/thinkerou/karate/grpc/ChannelFactory.java
@@ -1,9 +1,18 @@
 package com.thinkerou.karate.grpc;
 
+import io.grpc.Channel;
+import io.grpc.netty.shaded.io.grpc.netty.NegotiationType;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+
 /**
  * ChannelFactory
  *
  * @author thinkerou
  */
 public class ChannelFactory {
+
+    public Channel create(String host, int port) {
+        return NettyChannelBuilder.forAddress(host, port).negotiationType(NegotiationType.PLAINTEXT).build();
+    }
+
 }
