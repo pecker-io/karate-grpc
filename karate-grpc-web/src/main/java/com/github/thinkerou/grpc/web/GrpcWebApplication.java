569eb982dab3b54390f10bdd06e476707b1942aa
thinkerou <thinkerou@gmail.com>
2018-10-08 23:23:47 +0800

    web framework

---
+++ karate-grpc-web/src/main/java/com/github/thinkerou/grpc/web/GrpcWebApplication.java
@@ -0,0 +1,17 @@
+package com.github.thinkerou.grpc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GrpcWebApplication
+ *
+ * @author thinkerou
+ */
+@SpringBootApplication
+public class GrpcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcWebApplication.class, args);
+	}
+}
